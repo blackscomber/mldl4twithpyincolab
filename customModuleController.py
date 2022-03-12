340c74618f658038b62739d16c5a3df2ec20ed5e
blackscomber <toyosy@hotmail.com>
2022-03-12 10:24:26 +0000

    modified some codes for creating my module py files.

--- customModuleController.py
+++ customModuleController.py
@@ -16,24 +16,31 @@ import sys, os
 root
 Colab Notebooks: 1WXbbz1f0QEdltlsYEFpqt_21zm-vkDQq
 '''
-default_location='/content/notebooks'
+
+local_location='/content/notebooks'
+drive_github_location='/content/drive/MyDrive/ColabNotebooks/mldl4trading'
+current_location=local_location
+
+colab_folderId = '1WXbbz1f0QEdltlsYEFpqt_21zm-vkDQq'
+mldl4t_folderId = '1M8Db9NtqjU0_ybY49OCtZWldWT0C99d9'
+current_folderId = mldl4t_folderId
 
 class CustomModules():
-  def __init__(self, current_loaction:str=default_location):
+  def __init__(self, loaction:str=current_location):
     auth.authenticate_user()
     self._gauth = GoogleAuth()
     self._gauth.credentials = GoogleCredentials.get_application_default()
     self._drive = GoogleDrive(self._gauth)
-    self._currentLocation = current_loaction
+    self._currentLocation = loaction
     
   # Authenticate and create the PyDrive client.
   # This only needs to be done once per notebook.
-  def createFile(self, title : str, mimeType : str, parentId : str='1WXbbz1f0QEdltlsYEFpqt_21zm-vkDQq'):
+  def createFile(self, title : str, mimeType : str, parentId : str=current_folderId):
     
     try:
-      if(os.path.exists('/content/notebooks/{module}'.format(module=title))):
-      #with open('/content/notebooks/{module}'.format(module=title), 'r') as f:
-        print('| am already here444!!!')
+      #if(os.path.exists('{location}/{module}'.format(location=current_location, module=title))):
+      with open('{location}/{module}'.format(location=current_location, module=title), 'r') as f:
+        print('I am here at {location}/{module}'.format(location=current_location, module=title))
     except:
       module = self._drive.CreateFile({'title': title, 'mimeType':mimeType, 'parents':[{"kind": "drive#fileLink","id": parentId}] })
       print(module)
@@ -50,7 +57,7 @@ class CustomModules():
     
 
   # 'root' in parents for folderId is 'MyDrive'
-  def listModules(self, folderId:str='1WXbbz1f0QEdltlsYEFpqt_21zm-vkDQq'):
+  def listModules(self, folderId:str=current_folderId):
     file_list = self._drive.ListFile({'q': "'{folderId}' in parents".format(folderId=folderId)}).GetList()    
 
     for f in file_list:
