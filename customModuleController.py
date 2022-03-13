af8cd0d397d562e4bc249b43f312b28f79b5f4d0
blackscomber <toyosy@hotmail.com>
2022-03-13 11:14:21 +0000

    test to add .gitignore

--- customModuleController.py
+++ customModuleController.py
@@ -19,11 +19,12 @@ Colab Notebooks: 1WXbbz1f0QEdltlsYEFpqt_21zm-vkDQq
 
 local_location='/content/notebooks'
 drive_github_location='/content/drive/MyDrive/ColabNotebooks/mldl4trading'
-current_location=local_location
+current_location=drive_github_location
 
 colab_folderId = '1WXbbz1f0QEdltlsYEFpqt_21zm-vkDQq'
 mldl4t_folderId = '1M8Db9NtqjU0_ybY49OCtZWldWT0C99d9'
 current_folderId = mldl4t_folderId
+
 
 class CustomModules():
   def __init__(self, loaction:str=current_location):
@@ -36,25 +37,13 @@ class CustomModules():
   # Authenticate and create the PyDrive client.
   # This only needs to be done once per notebook.
   def createFile(self, title : str, mimeType : str, parentId : str=current_folderId):
-    
     try:
-      if(os.path.exists('{location}/{module}'.format(location=current_location, module=title))):
-      #with open('{location}/{module}'.format(location=current_location, module=title), 'r') as f:
-        print('I am here55 at {location}/{module}'.format(location=current_location, module=title))
+      if os.path.exists('{location}/{module}'.format(location=current_location, module=title)):
+        print('I am already here at {location}/{module}'.format(location=current_location, module=title))
     except:
       module = self._drive.CreateFile({'title': title, 'mimeType':mimeType, 'parents':[{"kind": "drive#fileLink","id": parentId}] })
       print(module)
       module.Upload()
-    
-    # module = self._drive.CreateFile({'title': title, 'mimeType':mimeType, 'parents':[{"kind": "drive#fileLink","id": parentId}] })
-    # module.get
-    # print(module)
-    # module.Upload()
-    # #if self._drive.get('title')
-    # t = GoogleDriveFile(self._gauth)
-    # t.Upload()
-
-    
 
   # 'root' in parents for folderId is 'MyDrive'
   def listModules(self, folderId:str=current_folderId):
