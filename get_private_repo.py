ee39c5cb4ee6b632edd52c6edeb3a9a0d110a779
blackscomber <toyosy@hotmail.com>
2022-03-14 09:28:48 +0000

    Add a new file to tell git who you are

--- get_private_repo.py
+++ get_private_repo.py
@@ -8,13 +8,10 @@ password = urllib.parse.quote(password) # your password is converted into url fo
 repo_name = input('Repo name: ')
 branch_name = input('Branch name: ')
 
-cmd_string = 'git push https://{user}:{password}@github.com/{user}/{repo_name}.git'.format(user=user, password=password, repo_name=repo_name)
-#git push https://<username>:<password>@github.com/<username>/<repository-name>
-
-# if branch_name == "master":
-#   cmd_string = 'git clone https://{user}:{password}@github.com/{user}/{repo_name}.git'.format(user=user, password=password, repo_name=repo_name)
-# else:
-#   cmd_string = 'git clone -b {branch_name} https://{user}:{password}@github.com/{user}/{repo_name}.git'.format(user=user, password=password, repo_name=repo_name, branch_name=branch_name)
+if branch_name == "master":
+  cmd_string = 'git clone https://{user}:{password}@github.com/{user}/{repo_name}.git'.format(user=user, password=password, repo_name=repo_name)
+else:
+  cmd_string = 'git clone -b {branch_name} https://{user}:{password}@github.com/{user}/{repo_name}.git'.format(user=user, password=password, repo_name=repo_name, branch_name=branch_name)
 
 os.system(cmd_string)
 cmd_string, password = "","" # removing the password from the variable
